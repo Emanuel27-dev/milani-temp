edc89cea007eee756d3c8656834a29cfe3bc7dd5
Enrique <xyxyxy27.79@gmail.com>
2025-10-16 10:31:30 -0500

    milani v.02

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,44 @@
-export function Home () {
-   return (<div>HOME</div>)
+// export function Home () {
+//    return (<div>HOME</div>)
+// }
+
+import { gql } from "@apollo/client"
+import { useQuery } from "@apollo/client/react"
+import DOMPurify from "dompurify";
+
+const NODE_BY_URI = gql`
+  query NodeByUri($uri: String!) {
+    nodeByUri(uri: $uri) {
+      __typename
+      ... on Page {
+        id
+        title
+        content
+        uri
+        slug
+      }
+    }
+  }
+`;
+
+export function Home() {
+  // Definimos directamente la URI de la página "Inicio"
+  const { data, loading, error } = useQuery(NODE_BY_URI, {
+    variables: { uri: "/inicio/" },
+  });
+
+  if (loading) return <p>Cargando contenido...</p>;
+  if (error) return <p>Error al cargar la página</p>;
+
+  const page = data?.nodeByUri;
+  if (!page) return <p>No se encontró la página de inicio</p>;
+
+  const safeHTML = DOMPurify.sanitize(page.content || "");
+
+  return (
+    <section>
+      <h1>{page.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: safeHTML }} />
+    </section>
+  );
 }
--- src/components/Header/HeaderLogo.jsx
+++ src/components/Header/HeaderLogo.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./styles/HeaderLogo.module.css";
 
 // componente para el logo (solo imagen)
-export function HeaderLogo ({ logo }) {
-   return (
+export function HeaderLogo({ logo }) {
+  return (
+    <Link to={"/"}>
       <figure className={styles.figure}>
-         <img src={logo} alt="Milani logo" className={styles.img} />
+        <img src={logo} alt="Milani logo" className={styles.img} />
       </figure>
-   )
+    </Link>
+  );
 }
