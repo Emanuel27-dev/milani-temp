925a94d116f0dd28d13eb3ab70c7bc267461ddeb
ivan <ivan.gutierrez2087@gmail.com>
2025-12-10 15:05:45 -0500

    fixed rollback

--- src/components/Header/ZipModal.jsx
+++ src/components/Header/ZipModal.jsx
@@ -1,57 +1,38 @@
 import layerLupa from "./../../assets/Layer.svg";
-import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
 
-export const GET_GEO_DATA = gql`
-  query {
-    wpcmRegions {
-      id
-      title
-      slug
-      cities {
-        name
-        slug
-        region
-      }
-    }
-    wpcmCities {
-      name
-      slug
-      region
-      regionTitle
-    }
-    posts {
-      nodes {
-        id
-        title
-        city {
-          name
-          slug
-          region
-          regionTitle
-        }
-      }
-    }
-  }
-`;
+const locations = [
+  {
+    region: "Okanagan",
+    cities: ["Kelowna", "West Kelowna", "Lake Country"],
+  },
+  {
+    region: "Alberta",
+    cities: [
+      "Grande Prairie",
+      "Fort McMurray",
+      "Peace River",
+      "High Level",
+      "Edmonton",
+      "St. Albert",
+    ],
+  },
+  {
+    region: "Lower Mainland",
+    cities: ["Vancouver", "Burnaby", "Richmond", "Surrey", "Coquitlam"],
+  },
+];
 
 export function ZipModal({
   isOpen,
   onClose,
   setCurrentLocation,
-  currentLocation,
 }) {
-  const { data, loading, error } = useQuery(GET_GEO_DATA);
-  if (loading) return <p></p>;
-  if (error) return <p>Error: {error.message}</p>;
 
-  const { wpcmRegions, wpcmCities, posts } = data;
   if (!isOpen) return null;
 
-  const handleItem = (city) => {
-    setCurrentLocation(city);
-    onClose();
-  };
+ const handleItem = (city) => {
+  setCurrentLocation(city)
+ }
 
   return (
     <div className="modal-overlay" onClick={onClose}>
@@ -67,29 +48,23 @@ export function ZipModal({
           </div>
 
           <div className="modal-body-custom">
-            {wpcmRegions
-              .filter(({ cities }) => cities.length > 0) // ⬅️ NO mostrar regiones vacías
-              .map(({ title, id, cities }) => (
-                <div key={id} className="modal-item">
-                  <h5 className="modal-item-title">{title}</h5>
+            {locations.map(({ region, cities }) => (
+              <div key={region} className="modal-item">
+                <h5 className="modal-item-title">{region}</h5>
 
-                  <div className="locations-grid">
-                    {cities.map(({ name }) => (
-                      <div
-                        key={name}
-                        className={`location-item ${
-                          currentLocation === name
-                            ? "active-currentLocation"
-                            : ""
-                        }`}
-                        onClick={() => handleItem(name)}
-                      >
-                        {name}
-                      </div>
-                    ))}
-                  </div>
+                <div className="locations-grid">
+                  {cities.map((city) => (
+                    <div 
+                      key={city}
+                      className="location-item"
+                      onClick={() => handleItem(city)}  
+                    >
+                      {city}
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
